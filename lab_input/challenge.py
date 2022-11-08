2580e64525923a23ec50138e9ea32da9c6bed250
nimagopali <nimagopali@gmail.com>
2022-11-08 20:58:05 +0000

    join and split

---
+++ lab_input/challenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+user_name = input("Please enter your name: ")                                        
+day = input("Please enter day of the week: ")                                     
+
+print("Hello,"+ user_name, "Happy"+ day, sep="!") 
+
+
+
+
