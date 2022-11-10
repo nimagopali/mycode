c10c645b9a921a84e929659c62eeda134e0a51d8
nimagopali <nimagopali@gmail.com>
2022-11-10 22:30:38 +0000

    studying for logic

---
+++ if_name/first_module.py
@@ -0,0 +1,9 @@
+def main():
+    print("Module #1 Name=", __name__)
+
+if __name__ == "__main__":
+    print("Code is being run directly from Python.")
+
+else:
+    print("Code is being run indirectly from import.")
+
